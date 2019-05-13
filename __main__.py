d7721d3d53bcbaf64f7c07b1af142e644d4df152
n4nel <n4nel@DESKTOP-2DTKP71>
2019-05-13 08:55:39 +0800

    fgd

--- __main__.py
+++ __main__.py
@@ -5,4 +5,5 @@ from DashAccidents import app
 
 # prevent execution when this module is imported by others
 if __name__ == "__main__":
-    app.server.run(debug=True, threaded=True, host='0.0.0.0', port=5000)
+
+    app.server.run(debug=True, threaded=True)
