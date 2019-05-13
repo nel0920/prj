b1962abd7c8926058d36256e58afe40315b523a2
n4nel <n4nel@DESKTOP-2DTKP71>
2019-05-13 09:20:28 +0800

    fd

--- app.py
+++ app.py
@@ -120,9 +120,9 @@ server = Flask(__name__, static_url_path='/DashAccidents/static')
 server.secret_key = os.environ.get('secret_key', 'secret')
 app = dash.Dash(__name__, server=server)#,  assets_external_path='https://raw.githubusercontent.com/nel0920/prj/master/DashAccidents/static/')
 app.config.supress_callback_exceptions = True
-server.config.from_object(Config)
+#server.config.from_object(Config)
 #app.config['GOOGLEMAPS_KEY'] = GOOGLEMAP
-server.config.from_object(DevelopmentConfig)
+#server.config.from_object(DevelopmentConfig)
 #GoogleMaps(server)
 
 # Include the external CSS
