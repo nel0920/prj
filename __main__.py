b0000651cef3e49ea2bf70a2d26faac70815146e
nel0920 <UseN@DESKTOP-Q4OAN0L.n4nel.local>
2019-05-13 17:53:45 +0800

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -5,4 +5,5 @@ from DashAccidents import app
 
 # prevent execution when this module is imported by others
 if __name__ == "__main__":
-    app.server.run(debug=True, threaded=True, host ='0.0.0.0', port=33599)
+	port = int(os.environ.get(“PORT”, 5000))
+    app.server.run(debug=True, threaded=True, host ='0.0.0.0', port=port)
