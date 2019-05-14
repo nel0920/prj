f93956b7fabba6fd5324e2583c490644f166f9a2
n4nel <n4nel@DESKTOP-2DTKP71>
2019-05-14 09:02:38 +0800

    Updated

--- DashAccidents/__init__.py
+++ DashAccidents/__init__.py
@@ -1,6 +1,6 @@
 import os
 import dash
-
+import dash_bootstrap_components as dbc
 from flask import Flask
 from DashAccidents.config import *
 
@@ -9,7 +9,7 @@ from DashAccidents.config import *
 # instantiate a Flask object
 server = Flask(__name__, static_url_path='/DashAccidents/static')
 server.secret_key = os.environ.get('secret_key', 'secret')
-app = dash.Dash(__name__, server=server)#,  assets_external_path='https://raw.githubusercontent.com/nel0920/prj/master/DashAccidents/static/')
+app = dash.Dash(__name__, server=server, external_stylesheets=[dbc.themes.BOOTSTRAP])#,  assets_external_path='https://raw.githubusercontent.com/nel0920/prj/master/DashAccidents/static/')
 app.config.supress_callback_exceptions = True
 server.config.from_object(Config)
 #app.config['GOOGLEMAPS_KEY'] = GOOGLEMAP
--- DashAccidents/views.py
+++ DashAccidents/views.py
@@ -9,10 +9,12 @@ from jinja2 import TemplateNotFound
 from random import randint
 
 import dash
-
-from dash.dependencies import Input, Output, State
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+from dash.dependencies import Input, Output, State
 
 from pandas import read_csv, read_excel, DataFrame
 
@@ -20,8 +22,6 @@ from flask import Flask, render_template
 from flask_googlemaps import GoogleMaps
 from flask_googlemaps import Map, icons
 
-
-from DashAccidents import server, app 
 from DashAccidents.config import *
 from DashAccidents.utils import *
 
@@ -39,10 +39,10 @@ from DashAccidents.utils import *
 #GoogleMaps(server)
 
 # Include the external CSS
-cssURL = "https://rawgit.com/richard-muir/uk-car-accidents/master/road-safety.css"
+cssUrl = ["https://rawgit.com/richard-muir/uk-car-accidents/master/road-safety.css", "https://use.fontawesome.com/releases/v5.8.2/css/all.css"]#, "https://codepen.io/chriddyp/pen/brPBPO.css"]
 #cssURL = 'DashAccidents/static/css/main.css'
 app.css.append_css({
-    "external_url": cssURL
+    "external_url": cssUrl
 })
 
 ## SETTING UP THE APP LAYOUT ##
@@ -55,7 +55,21 @@ app.css.append_css({
 
 app.layout = html.Div([html.H1('Traffic Accidents in the UK',
                 style={
+                    'width' : '100%', 
+                    'text-align': 'center',
+                    'fontFamily' : FONT_FAMILY
+                    }),            
+                html.Div([# Holds the map & the widgets
+
+                    dcc.Graph(id="map") # Holds the map in a div to apply styling to it
+                 
+                ],
+                style={
+                    "width" : '100%', 
+                    'display' : 'inline-block', 
+                    'paddingRight' : 50, 
                     'paddingLeft' : 50,
+                    'boxSizing' : 'border-box',
                     'fontFamily' : FONT_FAMILY
                     }),
                 html.Div([html.H3('''In 2017, the UK suffered {:,} traffic accidents, many of them fatal.'''.format(len(acc)),
@@ -88,7 +102,8 @@ app.layout = html.Div([html.H1('Traffic Accidents in the UK',
                 dcc.Checklist(# Checklist for the dats of week, sorted using the sorting dict created
                  # earlier
                     options=[
-                        {'label': age , 'value': age} for age in sorted(AGES)
+                        {'label': day[:3]# + ' [{:,}]'.format(len(list(filter(lambda k: k==day, acc['Day_of_Week']))))
+                         , 'value': day} for day in sorted(acc['Day_of_Week'].unique(), key=lambda k: DAYSORT[k])
                         #{'label': DAYSORT[day][:3], 'value': day} for day in sorted(DAYSORT)
                     ],
                     values=[day for day in acc['Day_of_Week'].unique()],
@@ -107,11 +122,12 @@ app.layout = html.Div([html.H1('Traffic Accidents in the UK',
                     }),
                 dcc.Checklist(# Checklist for the dats of week, sorted using the sorting dict created
                  # earlier
-                    options=[
-                        {'label': day[:3] + ' [{:,}]'.format(len(list(filter(lambda k: k==day, acc['Day_of_Week'])))), 'value': day} for day in sorted(acc['Day_of_Week'].unique(), key=lambda k: DAYSORT[k])
+                   id="ageChecklist",
+                   options=[
+                        {'label': AGES[age] , 'value': age} for age in sorted(AGES) if age >= 0
                         #{'label': DAYSORT[day][:3], 'value': day} for day in sorted(DAYSORT)
                     ],
-                    values=[day for day in acc['Day_of_Week'].unique()],
+                    values=[age for age in sorted(AGES) if age >= 0],
                     #values=[day for day in DAYSORT],
                     labelStyle={  # Different padding for the checklist elements
                         'display': 'inline-block',
@@ -119,7 +135,7 @@ app.layout = html.Div([html.H1('Traffic Accidents in the UK',
                         'paddingLeft' : 10,
                         'paddingBottom' : 5,
                         },
-                    id="ageChecklist",),
+                    ),
                 html.Div('''Select the hours in which the accident occurred (24h clock):''',
                     style={
                         'paddingTop' : 20,
@@ -134,25 +150,11 @@ app.layout = html.Div([html.H1('Traffic Accidents in the UK',
                     value=[acc['Hour'].min(), acc['Hour'].max()],
                     marks={str(h) : str(h) for h in range(acc['Hour'].min(), acc['Hour'].max() + 1)})],
                 style={
-                    "width" : '60%', 
+                    "width" : '100%', 
                     'display' : 'inline-block', 
                     'paddingLeft' : 50, 
                     'paddingRight' : 10,
                     'boxSizing' : 'border-box',
-                    }),            
-                html.Div([# Holds the map & the widgets
-
-                    dcc.Graph(id="map") # Holds the map in a div to apply styling to it
-                 
-                ],
-                style={
-                    "width" : '100%', 
-                    'float' : 'centre', 
-                    'display' : 'inline-block', 
-                    'paddingRight' : 50, 
-                    'paddingLeft' : 50,
-                    'boxSizing' : 'border-box',
-                    'fontFamily' : FONT_FAMILY
                     }),
                 html.Div([# Holds the heatmap & barchart (60:40 split)
                     html.Div([# Holds the heatmap
@@ -177,6 +179,47 @@ app.layout = html.Div([html.H1('Traffic Accidents in the UK',
                         'paddingLeft' : 5,
                         'boxSizing' : 'border-box'
                         })]),
+                dash_table.DataTable(
+                    id='datatable-interactivity',
+                    columns=[{"name": i, "id": i} for i in acc.columns],
+                    data=acc.to_dict('records'),
+                    editable=True,
+                    #filtering=True,
+                    sorting=True,
+                    sorting_type="multi",
+                    row_selectable="multi",
+                    #row_deletable=True,
+                    selected_rows=[],
+                    pagination_mode="fe",
+                        pagination_settings={
+                            "displayed_pages": 1,
+                            "current_page": 0,
+                            "page_size": 15,
+                        },
+                        navigation="page",
+                    css={
+                        'width' : '100%', 
+                        'paddingRight' : 50, 
+                        'paddingLeft' : 50,
+                        'display' : 'inline-block',
+                        'boxSizing' : 'border-box',
+                        'color': 'rgb(30, 30, 30)',  
+                        'fontFamily' : FONT_FAMILY,
+                    },
+                     style_as_list_view=True,
+                    style_header={
+                        'backgroundColor': 'rgb(30, 30, 30)',
+                        'fontWeight': 'bold'
+                    },
+                    style_cell={
+                        'padding': '5px',
+                        'backgroundColor': 'rgb(50, 50, 50)',
+                        'color': 'white'
+                    },
+                    style_table={                      
+                        'overflowX': 'scroll'
+                    }),
+                html.Div(id='datatable-interactivity-container'),
                 html.Div([# Add a source annotation and a note for the downsampling
                     html.Div('Source: https://data.gov.uk/dataset/road-accidents-safety-data',
                         style={
@@ -189,7 +232,12 @@ app.layout = html.Div([html.H1('Traffic Accidents in the UK',
                             'fontFamily' : FONT_FAMILY,
                             'fontSize' : 8,
                             'fontStyle' : 'italic'
-                        })])],
+                        })]),
+                dcc.Dropdown(
+                    id='dropdown',
+                    options=[{'label': i, 'value': i} for i in ['a', 'b', 'c']]
+                ),
+                html.Div(id='loading')],
                 style={'paddingBottom' : 20})
 
 #@app.callback(Output('tabs_content', 'children'),[Input('tabs', 'value')])
@@ -199,4 +247,14 @@ app.layout = html.Div([html.H1('Traffic Accidents in the UK',
 #        return []
 #    elif tab == 'tab_hk':
 #        return []
-        
+
+@app.callback(Output('loading', 'children'), [Input('dropdown', 'value')])
+def update_value(value):
+    time.sleep(2)
+    return 'You have computed {}'.format(value)
+
+# Dash CSS
+#app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
+
+# Loading screen CSS
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/brPBPO.css"})
